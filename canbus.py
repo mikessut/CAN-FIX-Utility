cb33c74917b7d6a0e4ba05ef98e751e1571441d1
Phil Birkelbach <phil@kubuntu.(none)>
2012-10-08 20:41:57 -0500

    Added exceptions to the CANBus class methods

--- canbus.py
+++ canbus.py
@@ -12,53 +12,87 @@ import time
 # we have and then set pointers to the right device classes so the rest
 # of the program doesn't have to worry about what device it is using
 
+class BusError(Exception):
+    """Base class for exceptions in this module"""
+
+    pass
+
+class BusInitError(BusError):
+    """CAN Bus Initialization Error"""
+
+    def __init__(self, msg):
+            self.msg = msg
+
+class BusReadError(BusError):
+    """CAN Bus Read Error"""
+
+    def __init__(self, msg):
+        self.msg = msg
+
+class BusWriteError(BusError):
+    """CAN Bus Write Error"""
+
+    def __init__(self, msg):
+        self.msg = msg
+
+class DeviceTimeout(Exception):
+    """Device Timeout Exception"""
+
+    pass
+
 class Connection():
     """Class that represents a connection to a CAN device"""
 
-    def readResponse(self):
+    def Easy_readResponse(self):
         str = ""
 
         while 1:
             x = self.ser.read()
             if len(x) == 0:
-                return -1
+                raise DeviceTimeout
             else:
                 str = str + x
                 if x == "\r": # Good Response
                     return str
                 if x == "\x07": # Bell is error
-                    return x
+                    raise BusReadError("USB2-F-7x01 Returned Bell")
 
     def Easy_Init(self):
         bitrates = {10:"S0\r", 20:"S1\r", 50:"S2\r", 100:"S3\r", 
                     125:"S4\r", 250:"S5\r", 500:"S6\r", 800:"S7\r", 1000:"S8\r"}
         print "Reseting USB2-F-7x01"
         self.ser.write("R\r")
-        result = self.readResponse()
-        if result != "\r":
-            print "Unable to Reset USB2-F-7x01"
-            exit(-1)
+        try:
+            result = self.readResponse()
+        except DeviceTimeout:
+            raise BusInitError("Timeout waiting for USB2-F-7x01")
+        except BusReadError:
+            raise BusInitError("Unable to Reset USB2-F-7x01")
         time.sleep(2)
 
         print "Setting Bit Rate"
         self.ser.write(bitrates[self.bitrate])
-        result = self.readResponse()
-        if result != "\r":
-            print "Unable to set CAN Bit rate"
-            exit(-1)
+        try:
+            result = self.readResponse()
+        except DeviceTimeout:
+            raise BusInitError("Timeout waiting for USB2-F-7x01")
+        except BusReadError:
+            raise BusInitError("Unable to set CAN Bit rate")
 
     def Easy_Open(self):
         print "Opening CAN Port"
         self.ser.write("O\r")
-        result = self.readResponse()
-        if result != "\r":
-            print "Unable to Open CAN Port"
-            exit(-1) #TODO: Raise an exception instead
-        
+        try:
+            result = self.readResponse()
+        except DeviceTimeout:
+            raise BusInitError("Timeout waiting for USB2-F-7x01")
+        except BusReadError:
+            raise BusInitError("Unable to Open CAN Port")
 
-    def __init__(self, portname = "", bitrate = 125):
+    def __init__(self, portname = "", bitrate = 125, timeout = 0.5):
         self.portname = portname
         self.bitrate = bitrate
+        self.timeout = timeout
 
     def connect(self):
         #This code tries to find a USB/CAN device on one of the comm ports.
@@ -69,13 +103,12 @@ class Connection():
                     print "Found CAN device on Port", self.portname
 
         if self.portname == "":
-            print "No CAN device found"
-            print comports()
-            exit(-1) #TODO: Raise an exception instead
+            raise BusInitError("No CAN device found")
 
-        self.ser = serial.Serial(self.portname, 115200, timeout=0.5)
+        self.ser = serial.Serial(self.portname, 115200, timeout=self.timeout)
         self.init = self.Easy_Init
         self.openport = self.Easy_Open
+        self.readResponse = self.Easy_readResponse
 
 
 
