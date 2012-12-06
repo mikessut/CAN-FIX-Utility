0e0017f2eaa014cf12a6dcadc0f9f8fd0077eae4
Phil Birkelbach <phil@petrasoft.net>
2012-12-06 14:45:41 -0600

    Add license to a few files.  Work on firmware dialog

--- src/canfix.py
+++ src/canfix.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+#  CAN-FIX Utilities - An Open Source CAN FIX Utility Package 
+#  Copyright (c) 2012 Phil Birkelbach
+#
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
 import sys
 import connection
 from PyQt4.QtCore import *
@@ -7,6 +24,8 @@ from PyQt4.QtGui import *
 import protocol
 from ui.main_ui import Ui_MainWindow
 from ui.connect_ui import Ui_ConnectDialog
+from ui.firmware_ui import Ui_dialogFirmware
+
 
 con = None
 
@@ -19,7 +38,11 @@ class connectDialog(QDialog, Ui_ConnectDialog):
         ports = con.getPortList()
         for each in ports:
             self.comboPort.addItem(each)
-
+            
+class dialogFirmware(QDialog, Ui_dialogFirmware):
+    def __init__(self):
+        QDialog.__init__(self)
+        self.setupUi(self)
 
 class modelData(QAbstractTableModel):
     def __init__(self):
@@ -137,7 +160,12 @@ class mainWindow(QMainWindow, Ui_MainWindow):
                 self.statusbar.showMessage("Failed to connect to %s" % port)
         else:
             print "Canceled"
-
+    
+    def loadFirmware(self):
+        connectDia = dialogFirmware()
+        x = connectDia.exec_()
+       
+                        
     def deviceEdit(self, index):
         self.devices.edit(index)
     
